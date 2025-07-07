0c636d3afabbdb0b2c7ff4a60bcfd69fa0846322
latttchc <iam.yuk2882@gmail.com>
2025-07-07 18:04:03 +0900

    feat:ブロッククラスを追加

---
+++ disciminator.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 4, stride, 1, bias=True, padding_mode="reflect"),
+            nn.InstanceNorm2d(out_channels),
+            nn.LeakyReLU(0.2),
+        )
